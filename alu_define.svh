fd3117bb62ad8272cdefa717b63011568c3c942a
rakshaanayak <138519058+rakshaanayak@users.noreply.github.com>
2024-08-03 12:16:24 +0530

    Add defines and timing parameters

--- alu_define.svh
+++ alu_define.svh
@@ -1,10 +1,14 @@
 //------------------------------------------------------------------------------
 // Project      : ALU 
 // File Name    : alu_define.svh
-// Developers   : 
+// Developers   : Team-2
 // Created Date : 01/08/2024
 // Version      : V1.0
 //------------------------------------------------------------------------------
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
 
+`timescale 10ns/100ps //Time Unit (ns) / Precision(ps)
+`define WIDTH 8
+`define NUM_TRANSACTIONS 4
+`define CMD_WIDTH 4

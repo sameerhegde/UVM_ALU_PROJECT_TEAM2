dbcec4b7ffe7e93212c6d8075bee6a83c99e62ac
vinodnikhil0506 <142741082+vinodnikhil0506@users.noreply.github.com>
2024-08-24 16:50:45 +0530

    Minor fixes

--- alu_sequence_item.sv
+++ alu_sequence_item.sv
@@ -8,50 +8,45 @@
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
 
-
 class alu_seq_item extends uvm_sequence_item;
 
-
-  rand bit [`WIDTH - 1:0]opa;
-  rand bit [`WIDTH - 1:0]opb;
-  rand bit  ce;
-  rand bit mode;
+  rand bit ce;
   rand bit cin;
-  rand bit [1:0]inp_valid;
-  rand bit [`CMD_WIDTH-1:0]cmd;
-  bit [`WIDTH:0] res;
-  bit  oflow,cout,g,l,e,err;
+  rand bit mode;
+  rand bit [1: 0] inp_valid;
+  rand bit [`CMD_WIDTH - 1:  0] cmd;
+  rand bit [`DATA_WIDTH - 1: 0] opa;
+  rand bit [`DATA_WIDTH - 1: 0] opb;
 
-`uvm_object_utils_begin(alu_seq_item)
-`uvm_field_int(opa,UVM_DEFAULT)
-`uvm_field_int(opb,UVM_DEFAULT)
-`uvm_field_int(ce,UVM_DEFAULT)
-`uvm_field_int(mode,UVM_DEFAULT)
-`uvm_field_int(cin,UVM_DEFAULT)
-`uvm_field_int(inp_valid,UVM_DEFAULT)
-`uvm_field_int(cmd,UVM_DEFAULT)
-`uvm_object_utils_end
+  bit [`DATA_WIDTH: 0] res;
+  bit oflow, cout, g, l, e, err;
 
-function new(string name ="alu_seq_item");
-super.new(name);
-endfunction
+  `uvm_object_utils_begin (alu_seq_item)
+    `uvm_field_int (ce,        UVM_DEFAULT)
+    `uvm_field_int (cin,       UVM_DEFAULT)
+    `uvm_field_int (cmd,       UVM_DEFAULT)
+    `uvm_field_int (opa,       UVM_DEFAULT)
+    `uvm_field_int (opb,       UVM_DEFAULT)
+    `uvm_field_int (mode,      UVM_DEFAULT)
+    `uvm_field_int (inp_valid, UVM_DEFAULT)
+  `uvm_object_utils_end
 
+  function new (string name = "alu_seq_item");
+    super.new(name);
+  endfunction
 
-constraint c1_mode
+  constraint c1_mode
   {
-    if( mode==0)cmd inside{[0:13]};
-    else cmd inside {[0:10]};
-                  
+    if( mode==0) cmd inside {[0: 13]};
+    else cmd inside {[0: 10]};               
   }
- constraint c2_inp_valid 
- {
-    inp_valid dist {0:=5,1:=10,2:=10,3:=10};
-                       
- }
-constraint c3_cmd 
- { 
-     cmd dist {[0:13]:=1};
-                  
- }
+  constraint c2_inp_valid 
+  {
+    inp_valid dist {0:=5, 1:=10, 2:=10, 3:=10};                     
+  }
+  constraint c3_cmd 
+  { 
+    cmd dist {[0: 13]:=1};                
+  }
 
 endclass

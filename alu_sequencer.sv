5ed21aaa222acf26dcd07f7c1a2a09d6d71cb89f
vinodnikhil0506 <142741082+vinodnikhil0506@users.noreply.github.com>
2024-08-24 16:56:22 +0530

    Fixed parameter value

--- alu_sequencer.sv
+++ alu_sequencer.sv
@@ -1,20 +1,19 @@
 //------------------------------------------------------------------------------
 // Project      : ALU 
 // File Name    : alu_sequencer.sv
-// Developers   :Nisha Shetty
- 
+// Developers   : Nisha Shetty
 // Created Date : 01/08/2024
 // Version      : V1.0
 //------------------------------------------------------------------------------
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
 
-class alu_seqr extends uvm_sequencer #(alu_sequence_item);
+class alu_seqr extends uvm_sequencer #(alu_seq_item);
 
-`uvm_object_utils(alu_seqr)
+  `uvm_object_utils (alu_seqr)
 
-function new(string name ="alu_seqr");
-super.name(name);
-endfunction
-
-endclass
+  function new (string name = "alu_seqr");
+    super.name (name);
+  endfunction
+  
+endclass: alu_seqr

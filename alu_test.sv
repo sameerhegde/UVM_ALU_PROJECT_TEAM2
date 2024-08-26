d82e216dd643787cee90a9fa0bfc4eccaae2f5d3
vinodnikhil0506 <142741082+vinodnikhil0506@users.noreply.github.com>
2024-08-26 11:51:55 +0530

    Added tests for arithmetic and logic ops

--- alu_test.sv
+++ alu_test.sv
@@ -45,3 +45,87 @@ class alu_test extends uvm_test;
 
 endclass: alu_test
 
+//------------------------------------------------------------------------------
+// Class         : arithmetic_ops_test
+// Description   : Test only arithmetic functionality
+//------------------------------------------------------------------------------
+
+class arithmetic_ops_test extends alu_test;
+  
+  // Factory registration
+  `uvm_component_utils (arithmetic_ops_test)
+
+  // Environment class handle
+  alu_env env_a;
+
+  // Base sequence handle
+  arithemtic_ops_seq seq;
+
+  // New function
+  function new (string name = "arithemtic_ops_test", uvm_component parent);
+    super.new (name, parent);
+  endfunction: new
+
+  // Build phase
+  virtual function void build_phase (uvm_phase phase);
+    super.build_phase (phase);
+    env_a = alu_env::type_id::create("env_a", this);
+  endfunction: build_phase
+
+  // End of elaboration phase
+  virtual function end_of_elaboration (uvm_phase phase);
+    // Print topology
+    print ():
+  endfunction: end_of_elaboration
+
+  // Run phase;
+  task run_phase (uvm_phase phase);
+    phase.raise_objection (this);
+    seq = arithmetic_ops_seq::type_id::create("seq");
+    phase.drop_objection (this);
+  endtask: run_phase
+
+endclass: arithmetic_ops_test
+
+//------------------------------------------------------------------------------
+// Class         : logical_ops_test
+// Description   : Test only arithmetic functionality
+//------------------------------------------------------------------------------
+
+class logical_ops_test extends alu_test;
+  
+  // Factory registration
+  `uvm_component_utils (logical_ops_test)
+
+  // Environment class handle
+  alu_env env_l;
+
+  // Base sequence handle
+  arithemtic_ops_seq seq;
+
+  // New function
+  function new (string name = "logical_ops_test", uvm_component parent);
+    super.new (name, parent);
+  endfunction: new
+
+  // Build phase
+  virtual function void build_phase (uvm_phase phase);
+    super.build_phase (phase);
+    env_l = alu_env::type_id::create("env_l", this);
+  endfunction: build_phase
+
+  // End of elaboration phase
+  virtual function end_of_elaboration (uvm_phase phase);
+    // Print topology
+    print ():
+  endfunction: end_of_elaboration
+
+  // Run phase;
+  task run_phase (uvm_phase phase);
+    phase.raise_objection (this);
+    seq = arithmetic_ops_seq::type_id::create("seq");
+    phase.drop_objection (this);
+  endtask: run_phase
+
+endclass: logical_ops_test
+

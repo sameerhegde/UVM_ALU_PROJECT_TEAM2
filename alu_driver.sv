b37d9b15f1e4cdf065b24e413da574011894f680
vinodnikhil0506 <142741082+vinodnikhil0506@users.noreply.github.com>
2024-08-26 10:34:58 +0530

    basic driver class

--- alu_driver.sv
+++ alu_driver.sv
@@ -8,3 +8,32 @@
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
 
+class alu_drv extends uvm_driver #(alu_seq_item);
+
+  `uvm_component_utils (alu_drv);
+
+  function new (string name = "alu_drv", uvm_component parent);
+    super.new (name, parent);
+  endfunction: new
+
+  virtual alu_if vif;  
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase (phase);
+    if (!uvm_config_db #(virtual alu_if)::get(this, "", "vif", vif))
+      `uvm_fatal ("No vif", {"Set virtual interface to: ", get_full_name (), ".vif"});
+  endfunction: build_phase
+
+  alu_seq_item txn
+
+  virtual task run_phase ();
+    repeat (`NUM_TRANSACTIONS) begin
+      seq_item_port.get_next_item (txn);
+      // drive
+      seq_item_port.item_done ();
+    end
+  endtask: run_phase
+
+  // drive task...
+
+endclass: alu_drv

637ac23e5d320d2f37d6ebed8530dc90f8e73ec3
AnweshRShetty <141901648+AnweshRShetty@users.noreply.github.com>
2024-08-26 12:49:41 +0530

    updated passive agent

--- alu_agent_passive.sv
+++ alu_agent_passive.sv
@@ -7,19 +7,22 @@
 //------------------------------------------------------------------------------
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
+
 class alu_agent_passive extends uvm_agent;
-'uvm_component_utils(alu_agent_active)
-function new(string name="alu_agent_passive",uvm_component parent=null);
-super.new(name,parent);
-endfunction
+ 
+ 'uvm_component_utils(alu_agent_active)
+  function new(string name="alu_agent_passive",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
 
-alu_monitor_read m1;
+  alu_monitor_read m1;
 
-
-virtual function void build_phase(uvm_phase phase);
-super.build_phase(phase);
-m0=alu_monitor_read::type_id::create("m1",this);
-endfunction
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(get_is_active() == UVM_PASSIVE) begin
+      m0=alu_monitor_read::type_id::create("m1",this);
+    end 
+  endfunction
 
 endclass
 

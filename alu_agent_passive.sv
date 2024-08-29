21781ecdee791c02be537d8b16476fa55d0998e6
Nisha Shetty <109810083+nishashetty11@users.noreply.github.com>
2024-08-29 15:07:51 +0530

    Updated handles

--- alu_agent_passive.sv
+++ alu_agent_passive.sv
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 // Project      : ALU 
 // File Name    : alu_agent_passive.sv
-// Developers   : 
+// Developers   : Nisha
 // Created Date : 01/08/2024
 // Version      : V1.0
 //------------------------------------------------------------------------------
@@ -10,17 +10,17 @@
 
 class alu_agent_passive extends uvm_agent;
  
- 'uvm_component_utils(alu_agent_active)
+ 'uvm_component_utils(alu_agent_passive)
   function new(string name="alu_agent_passive",uvm_component parent=null);
     super.new(name,parent);
   endfunction
 
-  alu_monitor_read m1;
+  alu_monitor_read mon_rd;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     if(get_is_active() == UVM_PASSIVE) begin
-      m1=alu_monitor_read::type_id::create("m1",this);
+     mon_rd=alu_monitor_read::type_id::create("mon_rd",this);
     end 
   endfunction
 

922d47cd8f5423ebff7f9526cf297c2afae2bbd5
AnweshRShetty <141901648+AnweshRShetty@users.noreply.github.com>
2024-09-02 16:00:58 +0530

    Update alu_environment.sv

--- alu_environment.sv
+++ alu_environment.sv
@@ -42,9 +42,9 @@ class alu_env extends uvm_env;
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
     act_agent.mon_w.item_collected_port.connect(sbh.ap_mon_wr);
-    act_agent.mon_w.item_collected_port.connect(cov.item_collected_export);
+    act_agent.mon_w.item_collected_port.connect(cov.mon_wr_imp);
     pass_agent.mon_r.item_collected_port.connect(sbh.ap_mon_rd);
-    pass_agent.mon_r.item_collected_port.connect(cov.item_collected_export); 
+    pass_agent.mon_r.item_collected_port.connect(cov.mon_rd_imp); 
   endfunction
 
 endclass: alu_env

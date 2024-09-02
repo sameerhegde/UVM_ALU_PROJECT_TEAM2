580f7745af8702e4583d83a6b586f7f5dcade4cf
pradyumnahegd <139676374+pradyumnahegd@users.noreply.github.com>
2024-09-02 16:33:22 +0530

    Updated alu_interface.sv

--- alu_interface.sv
+++ alu_interface.sv
@@ -8,41 +8,50 @@
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
 
-//`include "alu_define.svh"
+`include "alu_define.svh"
 
 interface alu_if (input bit clk, input bit rst);
   
   logic ce;
   logic cin;
   logic mode;
-  logic [1: 0] inp_valid;
-  logic [`CMD_WIDTH - 1:  0] cmd;
-  logic [`DATA_WIDTH - 1: 0] opa;
-  logic [`DATA_WIDTH - 1: 0] opb;
+  logic [1:0] inp_valid;
+  logic [`CMD_WIDTH - 1:0] cmd;
+  logic [`DATA_WIDTH - 1:0] opa;
+  logic [`DATA_WIDTH - 1:0] opb;
   
-  logic [`DATA_WIDTH: 0] res;
-  logic oflow, cout, g, l, e, err;
+  logic [`DATA_WIDTH:0] res;
+  logic oflow; 
+  logic cout; 
+  logic g; 
+  logic l; 
+  logic e; 
+  logic err;
   
   clocking drv_cb @(posedge clk or posedge rst);
     default input #0 output #0;
-    inout opa, opb, ce, mode, cin, inp_valid, cmd;
     input rst;
+    inout opa;
+    inout opb; 
+    inout ce; 
+    inout mode; 
+    inout cin; 
+    inout inp_valid; 
+    inout cmd;
   endclocking
   
   clocking mon_cb @(posedge clk);
     default input #0 output #0;
-    input res, oflow, cout, g, l, e, err;
+    input res;
+    input oflow; 
+    input cout; 
+    input g; 
+    input l; 
+    input e; 
+    input err;
   endclocking
-
-
-  /*clocking ref_cb @ (posedge clk or posedge rst);
-    default input #0 output #0;
-    input rst;
-  endclocking*/
   
   modport DRV (clocking drv_cb);
   modport MON (clocking mon_cb);
-
-  //modport REF (clocking ref_cb);
     
 endinterface

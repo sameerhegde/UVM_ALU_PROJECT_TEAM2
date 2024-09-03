6cb6fbb00599c0408393c5d0ec0ad0816b049db9
vinodnikhil0506 <142741082+vinodnikhil0506@users.noreply.github.com>
2024-09-03 12:41:00 +0530

    added package file - 5289

---
+++ alu_package.sv
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+// Project      : ALU 
+// File Name    : alu_package.sv
+// Developers   : Vinod (5289) 
+// Created Date : 03/09/2024
+// Version      : V1.0
+//------------------------------------------------------------------------------
+// Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
+//------------------------------------------------------------------------------
+
+`include "alu_sequence_item.v"
+`include "alu_sequence.sv"
+`include "alu_sequencer.sv"
+`include "alu_driver.sv"
+`include "alu_monitor_write.sv"
+`include "alu_monitor_read.sv"
+`include "alu_agent_active.sv"
+`include "alu_agent_passive.sv"
+`include "alu_scoreboard.sv"
+`include "alu_coverage.sv"
+`include "alu_environment.sv"
+`include "alu_test.sv"

389510cfde7f394ab1ccf5fc51036470cb28dce9
pradyumnahegd <139676374+pradyumnahegd@users.noreply.github.com>
2024-09-06 11:55:52 +0530

    Updated alu_scoreboard.sv

--- alu_scoreboard.sv
+++ alu_scoreboard.sv
@@ -1,262 +1,270 @@
 //------------------------------------------------------------------------------
 // Project      : ALU 
 // File Name    : alu_scoreboard.sv
-// Developers   : Vinod (5289)
+// Developers   : Team-2
 // Created Date : 01/08/2024
 // Version      : V1.0
 //------------------------------------------------------------------------------
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
 
-`uvm_analysis_imp_decl (_mon_wr)
-`uvm_analysis_imp_decl (_mon_rd)
+`uvm_analysis_imp_decl (_ip_mon)
+`uvm_analysis_imp_decl (_op_mon)
 
 class alu_scb extends uvm_scoreboard;
   
   // Factory registration
   `uvm_component_utils (alu_scb)
-
+  
   // Class constructor
   function new (string name = "alu_scb", uvm_component parent);
-    super.new (name);
+    super.new(name,parent);
   endfunction: new
-
-  // Analysis implemenatation ports
-  uvm_analysis_imp_mon_wr #(alu_seq_item, alu_scb) ap_mon_wr;
-  uvm_analysis_imp_mon_rd #(alu_seq_item, alu_scb) ap_mon_rd;
-
-  // FIFOs
-  uvm_tlm_fifo #(alu_seq_item) wr_txn_fifo
-  uvm_tlm_fifo #(alu_seq_item) rd_txn_fifo
-  //alu_seq_item wr_txn_queue[$];
-  //alu_seq_item rd_txn_queue[$];
-
-  // Virtual interface handle
+  
   virtual alu_if vif;
+  // Analysis implemenatation ports
+  uvm_analysis_imp_ip_mon #(alu_seq_item, alu_scb) ip_mon_port;
+  uvm_analysis_imp_op_mon #(alu_seq_item, alu_scb) op_mon_port;
+
+  alu_seq_item ip_queue[$];
+  alu_seq_item op_queue[$];
 
   // Count variable for compare status and total tests ran
-  int pass, fail, total;
+  //int pass, fail, total;
 
   // Build phase
   function void build_phase (uvm_phase phase);
     super.build_phase (phase);
-    ap_mon_wr = new ("ap_mon_wr", this);
-    ap_mon_rd = new ("ap_mon_rd", this);
-    wr_txn_fifo = new ("wr_txn_fifo", this);
-    rd_txn_fifo = new ("rd_txn_fifo", this);
+    ip_mon_port = new ("ip_mon_port", this);
+    op_mon_port = new ("op_mon_port", this);
+    if (!uvm_config_db #(virtual alu_if)::get(this, "", "vif", vif))
+      `uvm_fatal ("No vif", {"Set virtual interface to: ", get_full_name (), ".vif"});
   endfunction: build_phase
  
   // Do something with the monitor_write txn
-  virtual function void write_mon_wr (alu_seq_item txn);
-    if (vif.rst || txn.ce == 0) begin
-      txn.res = 'bz;
-      txn.cout = 'bz;
-      txn.oflow = 'bz;
-      txn.g = 'bz;
-      txn.l = 'bz;
-      txn.e = 'bz;
-      txn.err = 'bz;
-    end
-    else if (txn.ce == 1) begin
-      if (txn.mode == 1) begin
-        // arithmetic checkers...
-        case (txn.cmd)
-          0: begin
-            if (txn.inp_valid == 2'b11)
-              txn.res = txn.opa + txn.opb;
-            else
-              txn.res = 'bz;
-          end
-          1: begin
-            if (txn.inp_valid == 2'b11)
-              txn.res = txn.opa - txn.opb;
-            else
-              txn.res = 'bz;
-          end
-          2: begin
-            if (txn.inp_valid == 2'b11)
-              txn.res = txn.opa + txn.opb + txn.cin;
-            else
-              txn.res = 'bz;
-          end
-          3: begin
-            if (txn.inp_valid == 2'b11)
-              txn.res = txn.opa - txn.opb - txn.cin;
-            else
-              txn.res = 'bz;
-          end
-          4: begin
-            if (txn.inp_valid == 2'b01)
-              txn.res = txn.opa + 1'b1;
-            else
-              txn.res = 'bz;
-          end
-          5: begin
-            if (txn.inp_valid == 2'b01)
-              txn.res = txn.opa - 1'b1;
-            else
-              txn.res = 'bz;
-          end
-          6: begin
-            if (txn.inp_valid == 2'b10)
-              txn.res = txn.opb + 1'b1;
-            else
-              txn.res = 'bz;
-          end
-          7: begin
-            if (txn.inp_valid == 2'b10)
-              txn.res = txn.opb - 1'b1;
-            else
-              txn.res = 'bz;
-          end
-          8: begin
-            if (txn.inp_valid == 2'b11) begin
-              if (txn.opa > txn.opb)  begin
-                txn.g = 'b1; txn.l = 'b0; txn.e = 'b0;
-              end
-              else if (txn.opa < txn.opb) begin
-                txn.g = 'b0; txn.l = 'b1; txn.e = 'b0;
-              end
-              else begin
-                txn.g = 'b0; txn.l = 'b0; txn.e = 'b1;
+  virtual function void write_ip_mon (alu_seq_item item);
+    ip_queue.push_back(item);
+  endfunction
+  
+  virtual function void write_op_mon (alu_seq_item item);
+    op_queue.push_back(item);
+  endfunction
+    
+  task run_phase (uvm_phase phase);
+    super.run_phase(phase);
+   
+    forever 
+      begin
+        alu_seq_item exp_trans,act_trans;
+        
+        wait(ip_queue.size() > 0 && op_queue.size() > 0);
+        exp_trans = ip_queue.pop_front();
+        act_trans = op_queue.pop_front();
+        
+        compare(exp_trans,act_trans);
+        
+      end
+  
+endtask
+
+task compare(alu_seq_item exp_trans,alu_seq_item act_trans);
+  
+  if(exp_trans.ce ==1)
+    begin
+      if(exp_trans.mode ==1 )
+        begin
+          case(exp_trans.cmd)
+            0:begin
+              if(exp_trans.inp_valid == 2'b11)
+                exp_trans.res = exp_trans.opa + exp_trans.opb;
+              else
+                exp_trans.res = 'bz;
+            end
+            1:begin
+              if(exp_trans.inp_valid == 2'b11)
+                exp_trans.res = exp_trans.opa - exp_trans.opb;
+              else
+                exp_trans.res = 'bz;
+            end
+            2:begin
+              if (exp_trans.inp_valid == 2'b11)
+                exp_trans.res = exp_trans.opa + exp_trans.opb + exp_trans.cin;
+              else
+                exp_trans.res = 'bz;
+            end
+            3:begin
+              if (exp_trans.inp_valid == 2'b11)
+                exp_trans.res = exp_trans.opa - exp_trans.opb - exp_trans.cin;
+              else      
+                exp_trans.res = 'bz;
+            end
+            4:begin
+              if (exp_trans.inp_valid == 2'b01)
+                  exp_trans.res = exp_trans.opa + 1'b1;
+                else
+                  exp_trans.res = 'bz;
+            end
+            5:begin
+              if (exp_trans.inp_valid == 2'b01)
+                  exp_trans.res = exp_trans.opa - 1'b1;
+                else
+                  exp_trans.res = 'bz;
+            end
+            6:begin
+              if (exp_trans.inp_valid == 2'b10)
+                  exp_trans.res = exp_trans.opb + 1'b1;
+                else
+                  exp_trans.res = 'bz;
+            end
+            7:begin
+              if (exp_trans.inp_valid == 2'b10)
+                  exp_trans.res = exp_trans.opb - 1'b1;
+                else
+                  exp_trans.res = 'bz;
+            end
+            8:begin
+              if (exp_trans.inp_valid == 2'b11) begin
+                if (exp_trans.opa > exp_trans.opb)  begin
+                  exp_trans.g = 'b1; exp_trans.l = 'b0; exp_trans.e = 'b0;
+                end
+                else if (exp_trans.opa < exp_trans.opb) begin
+                  exp_trans.g = 'b0; exp_trans.l = 'b1; exp_trans.e = 'b0;
+                end
+                else begin
+                  exp_trans.g = 'b0; exp_trans.l = 'b0; exp_trans.e = 'b1;
+                end
               end
             end
-          end
-          9: begin
-             if (txn.inp_valid == 2'b11)
-               txn.res = (txn.opa + 1'b1) * (txn.opb + 1'b1);
-             else
-               txn.res = 'bz;
-          end
-          10: begin
-            if (txn.inp_valid == 2'b11)
-              txn.res = (txn.opa << 1) * txn.opb;
-            else
-              txn.res = 'bz;
-          end
-          default: txn.res = 'bz;
-        endcase        
-      end
-      else begin
-        case(txn.cmd)
-           0: AND
-             begin
-               if(txn.inp_valid == 2'b11)
-                  txn.res = txn.opa && txn.opb;
-               else
-                  txn.res = 'bz;
-             end
-           1: NAND
-             begin
-                if(txn.inp_valid == 2'b11)
-                  txn.res = ~(txn.opa && txn.opb);
-               else
-                  txn.res = 'bz;
-             end
-           2: OR
-             begin
-                if(txn.inp_valid == 2'b11)
-                  txn.res = txn.opa || txn.opb;
-               else
-                  txn.res = 'bz;
-             end
-           3: NOR
-             begin
-                if(txn.inp_valid == 2'b11)
-                  txn.res = ~(txn.opa || txn.opb);
-               else
-                  txn.res = 'bz;
-             end
-           4: XOR
-             begin
-                if(txn.inp_valid == 2'b11)
-                  txn.res = txn.opa ^ txn.opb;
-               else
-                  txn.res = 'bz;
-             end
-           5: XNOR
-             begin
-                if(txn.inp_valid == 2'b11)
-                  txn.res = ~(txn.opa ^ txn.opb);
-               else
-                  txn.res = 'bz;
-             end
-           6:
-             begin
-                if(txn.inp_valid == 2'b01)
-                  txn.res = ~ txn.opa;
-               else
-                  txn.res = 'bz;
-             end
-            7:
-             begin
-                if(txn.inp_valid == 2'b10)
-                  txn.res = ~ txn.opb;
-               else
-                  txn.res = 'bz;
-             end
-             8:
-             begin
-                if(txn.inp_valid == 2'b01)
-                  txn.res =  txn.opa >> 1;
-               else
-                  txn.res = 'bz;
-             end
-              9:
-             begin
-                if(txn.inp_valid == 2'b01)
-                  txn.res =  txn.opa << 1;
-               else
-                  txn.res = 'bz;
-             end
-              10:
-             begin
-                if(txn.inp_valid == 2'b10)
-                  txn.res =  txn.opb >> 1;
-               else
-                  txn.res = 'bz;
-             end
-              11:
-             begin
-                if(txn.inp_valid == 2'b10)
-                  txn.res =  txn.opb << 1;
-               else
-                  txn.res = 'bz;
-             end       
-
-      end
+            9:begin
+              if (exp_trans.inp_valid == 2'b11)
+                exp_trans.res = (exp_trans.opa + 1'b1) * (exp_trans.opb + 1'b1);
+              else
+                exp_trans.res = 'bz;              
+            end
+            10:begin
+              if (exp_trans.inp_valid == 2'b11)
+                exp_trans.res = (exp_trans.opa << 1) * exp_trans.opb;
+              else
+                exp_trans.res = 'bz;
+            end
+            default: exp_trans.res = 'bz;
+          endcase
+        end
+      else
+        begin
+          case(exp_trans.cmd)
+            0:begin
+              if(exp_trans.inp_valid == 2'b11)
+                exp_trans.res = exp_trans.opa && exp_trans.opb;
+              else
+                exp_trans.res = 'bz;
+            end
+            1:begin
+              if(exp_trans.inp_valid == 2'b11)
+                exp_trans.res = ~(exp_trans.opa && exp_trans.opb);
+              else
+                exp_trans.res = 'bz;
+            end
+            2:begin
+              if(exp_trans.inp_valid == 2'b11)
+                exp_trans.res = exp_trans.opa || exp_trans.opb;
+              else
+                exp_trans.res = 'bz;
+            end
+            3:begin
+              if(exp_trans.inp_valid == 2'b11)
+                exp_trans.res = ~(exp_trans.opa || exp_trans.opb);
+              else
+                exp_trans.res = 'bz;
+            end
+            4:begin
+              if(exp_trans.inp_valid == 2'b11)
+                exp_trans.res = exp_trans.opa ^ exp_trans.opb;
+              else
+                exp_trans.res = 'bz;
+            end
+            5:begin
+              if(exp_trans.inp_valid == 2'b11)
+                exp_trans.res = ~(exp_trans.opa ^ exp_trans.opb);
+              else
+                exp_trans.res = 'bz;
+            end
+            6:begin
+              if(exp_trans.inp_valid == 2'b01)
+                exp_trans.res = ~ exp_trans.opa;
+              else
+                exp_trans.res = 'bz;
+            end
+            7:begin
+              if(exp_trans.inp_valid == 2'b10)
+                exp_trans.res = ~ exp_trans.opb;
+              else
+                exp_trans.res = 'bz;
+            end
+            8:begin
+              if(exp_trans.inp_valid == 2'b01)
+                exp_trans.res =  exp_trans.opa >> 1;
+              else
+                exp_trans.res = 'bz;
+            end
+            9:begin
+              if(exp_trans.inp_valid == 2'b01)
+                exp_trans.res =  exp_trans.opa << 1;
+              else
+                exp_trans.res = 'bz;
+            end
+            10:begin
+              if(exp_trans.inp_valid == 2'b10)
+                exp_trans.res =  exp_trans.opb >> 1;
+              else
+                exp_trans.res = 'bz;
+            end
+            11:begin
+              if(exp_trans.inp_valid == 2'b10)
+                exp_trans.res =  exp_trans.opb << 1;
+              else
+                exp_trans.res = 'bz;
+            end
+          endcase
+        end
     end
-    wr_txn_fifo.try_put (txn);
-  endfunction: write_mon_wr
   
-  // Do something with the monitor_read txn
-  virtual function void write_mon_rd (alu_seq_item txn);
-    rd_txn_fifo.try_put (txn);
-  endfunction: write_mon_rd
+  if(exp_trans.res == act_trans.res)
+    $display("COMPARE %s ", $sformatf("Transaction Passed! ACT=%d, EXP=%d", act_trans.res, exp_trans.res));
+  else
+    $display("COMPARE", $sformatf("Transaction failed! ACT=%d, EXP=%d",  act_trans.res, exp_trans.res));
+    
+  if(exp_trans.cout == act_trans.cout)
+    `uvm_info("COMPARE", $sformatf("Transaction Passed! ACT=%d, EXP=%d", act_trans.cout, exp_trans.cout), UVM_LOW)
+  else
+    `uvm_error("COMPARE", $sformatf("Transaction failed! ACT=%d, EXP=%d",  act_trans.cout, exp_trans.cout))  
+  
+  if(exp_trans.oflow == act_trans.oflow)
+    `uvm_info("COMPARE", $sformatf("Transaction Passed! ACT=%d, EXP=%d", act_trans.oflow, exp_trans.oflow), UVM_LOW)
+  else
+    `uvm_error("COMPARE", $sformatf("Transaction failed! ACT=%d, EXP=%d",  act_trans.oflow, exp_trans.oflow)) 
+    
+  if(exp_trans.g == act_trans.g)
+    `uvm_info("COMPARE", $sformatf("Transaction Passed! ACT=%d, EXP=%d", act_trans.g, exp_trans.g), UVM_LOW)
+  else
+    `uvm_error("COMPARE", $sformatf("Transaction failed! ACT=%d, EXP=%d",  act_trans.g, exp_trans.g))
+  
+  if(exp_trans.l == act_trans.l)
+    `uvm_info("COMPARE", $sformatf("Transaction Passed! ACT=%d, EXP=%d", act_trans.l, exp_trans.l), UVM_LOW)
+  else
+    `uvm_error("COMPARE", $sformatf("Transaction failed! ACT=%d, EXP=%d",  act_trans.l, exp_trans.l))  
+    
+  if(exp_trans.err == act_trans.err)
+    `uvm_info("COMPARE", $sformatf("Transaction Passed! ACT=%d, EXP=%d", act_trans.err, exp_trans.err), UVM_LOW)
+  else
+    `uvm_error("COMPARE", $sformatf("Transaction faierred! ACT=%d, EXP=%d",  act_trans.err, exp_trans.err))
+    
+  if(exp_trans.e == act_trans.e)
+    `uvm_info("COMPARE", $sformatf("Transaction Passed! ACT=%d, EXP=%d", act_trans.e, exp_trans.e), UVM_LOW)
+  else
+    `uvm_error("COMPARE", $sformatf("Transaction faieed! ACT=%d, EXP=%d",  act_trans.e, exp_trans.e))
+    
+endtask
 
-  // Run phase
-  virtual task run_phase (uvm_phase phase);
-    alu_seq_item act_txn;
-    alu_seq_item exp_txn;
-    repeat (`NUM_TRANSACTIONS) begin
-      wr_txn_queue.get (exp_txn);
-      rd_txn_queue.get (act_txn);
-      // Compare
-
-      // Call test_pass(), test_fail() functions
-    end
-  endtask: run_phase
-
-  // Test has compared
-  function void test_pass;
-    pass++;
-    total++;
-  endfunction: test_pass
-
-  // Test has failed
-  function void test_fail;
-    fail++;
-    total++;
-  endfunction: test_fail
-
-endclass: alu_scb
+  
+endclass

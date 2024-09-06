60d9961888a4a0f5321175dc4d64f7cfd5210cd7
pradyumnahegd <139676374+pradyumnahegd@users.noreply.github.com>
2024-09-06 11:51:56 +0530

    Updated alu_top.sv

--- alu_top.sv
+++ alu_top.sv
@@ -1,18 +1,18 @@
 //------------------------------------------------------------------------------
 // Project      : ALU 
 // File Name    : alu_top.sv
-// Developers   : Raksha Nayak 
+// Developers   : Team-2
 // Created Date : 01/08/2024
 // Version      : V1.0
 //------------------------------------------------------------------------------
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 
-`include "alu_design.v"
-`include "alu_interface.sv"
-`include "alu_package.sv"
+
+`include "package.sv"
 
 module top ();
+   
     bit clk,rst;
 
     always #5 clk = ~clk;
@@ -33,7 +33,7 @@ module top ();
            .RST(intf.rst),
            .CE(intf.ce),
            .MODE(intf.mode),
-           .INP_VALID(intf,inp_valid),
+      	   .INP_VALID(intf.inp_valid),
            .CMD(intf.cmd),
            .COUT(intf.cout),
            .RES(intf.res),
@@ -46,12 +46,11 @@ module top ();
 
      initial begin 
        uvm_config_db #(virtual alu_if)::set(uvm_root::get(),"*","vif",intf);
-    
      end
      
      initial begin
 
-       run_test("alu_test");
+       run_test("alu_add");
 
      end
 endmodule

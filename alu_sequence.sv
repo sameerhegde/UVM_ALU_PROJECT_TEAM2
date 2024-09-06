4de6b0c7a5f188f6f6a5d6427fcd8fb140c491e5
AnweshRShetty <141901648+AnweshRShetty@users.noreply.github.com>
2024-09-06 11:53:48 +0530

    Update alu_sequence.sv

--- alu_sequence.sv
+++ alu_sequence.sv
@@ -8,8 +8,6 @@
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
 
-`include "alu_sequence_item.sv"
-
 class alu_seq extends uvm_sequence #(alu_seq_item);
 
   // Factory registration
@@ -29,7 +27,7 @@ class alu_seq extends uvm_sequence #(alu_seq_item);
       wait_for_grant ();
       mainSeq: assert (txn.randomize)
       else $error ("%m Randomization failed!");
-      send_txnuest (txn);
+      send_request (txn);
       wait_for_item_done ();
     end
   endtask: body
@@ -59,421 +57,421 @@ class alu_add_sequence extends alu_seq;
   endtask
 endclass
 
-class alu_sub_sequence extends alu_seq;
+// class alu_sub_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_sub_sequence)
+//   `uvm_object_utils(alu_sub_sequence)
 
-  function new(string name = "alu_sub_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_sub_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b0001;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b0001;
+//     })
+//   endtask
+// endclass
 
-class alu_add_cin_sequence extends alu_seq;
+// class alu_add_cin_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_add_cin_sequence)
+//   `uvm_object_utils(alu_add_cin_sequence)
 
-  function new(string name = "alu_add_cin_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_add_cin_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b0010;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b0010;
+//     })
+//   endtask
+// endclass
 
-class alu_sub_cin_sequence extends alu_seq;
+// class alu_sub_cin_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_sub_cin_sequence)
+//   `uvm_object_utils(alu_sub_cin_sequence)
 
-  function new(string name = "alu_sub_cin_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_sub_cin_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b0011;
-    })
-  endtask
-endclass
-
-
-
-class alu_inc_op1_sequence extends alu_seq;
-
-  `uvm_object_utils(alu_inc_op1_sequence)
-
-  function new(string name = "alu_inc_op1_sequence");
-    super.new(name);
-  endfunction
-
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b0100;
-    })
-  endtask
-endclass
-
-class alu_dec_op1_sequence extends alu_seq;
-
-  `uvm_object_utils(alu_dec_op1_sequence)
-
-  function new(string name = "alu_dec_op1_sequence");
-    super.new(name);
-  endfunction
-
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b0101;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b0011;
+//     })
+//   endtask
+// endclass
 
 
-class alu_inc_op2_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_inc_op2_sequence)
+// class alu_inc_op1_sequence extends alu_seq;
 
-  function new(string name = "alu_inc_op2_sequence");
-    super.new(name);
-  endfunction
+//   `uvm_object_utils(alu_inc_op1_sequence)
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b0110;
-    })
-  endtask
-endclass
+//   function new(string name = "alu_inc_op1_sequence");
+//     super.new(name);
+//   endfunction
 
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b0100;
+//     })
+//   endtask
+// endclass
 
-class alu_dec_op2_sequence extends alu_seq;
+// class alu_dec_op1_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_dec_op2_sequence)
+//   `uvm_object_utils(alu_dec_op1_sequence)
 
-  function new(string name = "alu_dec_op2_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_dec_op1_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b0111;
-    })
-  endtask
-endclass
-
-
-class alu_cmp_sequence extends alu_seq;
-
-  `uvm_object_utils(alu_cmp_sequence)
-
-  function new(string name = "alu_cmp_sequence");
-    super.new(name);
-  endfunction
-
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd == 4'b1000;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b0101;
+//     })
+//   endtask
+// endclass
 
 
-class alu_inc_mul_sequence extends alu_seq;
+// class alu_inc_op2_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_inc_mul_sequence)
+//   `uvm_object_utils(alu_inc_op2_sequence)
 
-  function new(string name = "alu_inc_mul_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_inc_op2_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b1001;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b0110;
+//     })
+//   endtask
+// endclass
 
-class alu_opa_lshift_mul_op1_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_opa_lshift_mul_sequence)
+// class alu_dec_op2_sequence extends alu_seq;
 
-  function new(string name = "alu_opa_lshift_mul_sequence");
-    super.new(name);
-  endfunction
+//   `uvm_object_utils(alu_dec_op2_sequence)
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b1;
-      txn.cmd ==4'b1010;
-    })
-  endtask
-endclass
+//   function new(string name = "alu_dec_op2_sequence");
+//     super.new(name);
+//   endfunction
 
-//------------------------------------------------------------------------------
-// Class       : logical_ops_seq
-// Description : Only logical operations
-//------------------------------------------------------------------------------
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b0111;
+//     })
+//   endtask
+// endclass
 
-class logical_ops_seq extends alu_seq;
+
+// class alu_cmp_sequence extends alu_seq;
+
+//   `uvm_object_utils(alu_cmp_sequence)
+
+//   function new(string name = "alu_cmp_sequence");
+//     super.new(name);
+//   endfunction
+
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd == 4'b1000;
+//     })
+//   endtask
+// endclass
+
+
+// class alu_inc_mul_sequence extends alu_seq;
+
+//   `uvm_object_utils(alu_inc_mul_sequence)
+
+//   function new(string name = "alu_inc_mul_sequence");
+//     super.new(name);
+//   endfunction
+
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b1001;
+//     })
+//   endtask
+// endclass
+
+// class alu_opa_lshift_mul_op1_sequence extends alu_seq;
+
+//   `uvm_object_utils(alu_opa_lshift_mul_sequence)
+
+//   function new(string name = "alu_opa_lshift_mul_sequence");
+//     super.new(name);
+//   endfunction
+
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b1;
+//       txn.cmd ==4'b1010;
+//     })
+//   endtask
+// endclass
+
+// //------------------------------------------------------------------------------
+// // Class       : logical_ops_seq
+// // Description : Only logical operations
+// //------------------------------------------------------------------------------
+
+// class logical_ops_seq extends alu_seq;
   
-  // Factory registration
-  `uvm_object_utils (logical_ops_seq)
+//   // Factory registration
+//   `uvm_object_utils (logical_ops_seq)
 
-  // Class constructor
-  function new (string name = "logical_ops_seq");
-    super.new (name);
-  endfunction: new
+//   // Class constructor
+//   function new (string name = "logical_ops_seq");
+//     super.new (name);
+//   endfunction: new
 
-  // Handle for alu_seq_item
-  alu_seq_item txn;
+//   // Handle for alu_seq_item
+//   alu_seq_item txn;
 
-  virtual task body;
-    repeat (`NUM_TRANSACTIONS) begin
-      `uvm_do_with (txn, {txn.mode == 0;});
-    end
-  endtask: body
+//   virtual task body;
+//     repeat (`NUM_TRANSACTIONS) begin
+//       `uvm_do_with (txn, {txn.mode == 0;});
+//     end
+//   endtask: body
 
-endclass: logical_ops_seq
+// endclass: logical_ops_seq
 
 
-class alu_and_sequence extends alu_seq;
+// class alu_and_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_and_sequence)
+//   `uvm_object_utils(alu_and_sequence)
 
-  function new(string name = "alu_and_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_and_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b0000;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b0000;
+//     })
+//   endtask
+// endclass
 
-class alu_nand_sequence extends alu_seq;
+// class alu_nand_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_nand_sequence)
+//   `uvm_object_utils(alu_nand_sequence)
 
-  function new(string name = "alu_nand_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_nand_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b0001;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b0001;
+//     })
+//   endtask
+// endclass
 
-class alu_or_sequence extends alu_seq;
+// class alu_or_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_or_sequence)
+//   `uvm_object_utils(alu_or_sequence)
 
-  function new(string name = "alu_or_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_or_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b0010;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b0010;
+//     })
+//   endtask
+// endclass
 
-class alu_nor_sequence extends alu_seq;
+// class alu_nor_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_nor_sequence)
+//   `uvm_object_utils(alu_nor_sequence)
 
-  function new(string name = "alu_nor_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_nor_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b0011;
-    })
-  endtask
-endclass
-class alu_xor_sequence extends alu_seq;
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b0011;
+//     })
+//   endtask
+// endclass
+// class alu_xor_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_xor_sequence)
+//   `uvm_object_utils(alu_xor_sequence)
 
-  function new(string name = "alu_xor_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_xor_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b0100;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b0100;
+//     })
+//   endtask
+// endclass
 
-class alu_xnor_sequence extends alu_seq;
+// class alu_xnor_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_xnor_sequence)
+//   `uvm_object_utils(alu_xnor_sequence)
 
-  function new(string name = "alu_xnor_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_xnor_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b0101;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b0101;
+//     })
+//   endtask
+// endclass
 
-class alu_not_op1_sequence extends alu_seq;
+// class alu_not_op1_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_not_op1_sequence)
+//   `uvm_object_utils(alu_not_op1_sequence)
 
-  function new(string name = "alu_not_op1_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_not_op1_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b0110;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b0110;
+//     })
+//   endtask
+// endclass
 
-class alu_not_op2_sequence extends alu_seq;
+// class alu_not_op2_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_not_op2_sequence)
+//   `uvm_object_utils(alu_not_op2_sequence)
 
-  function new(string name = "alu_not_op2_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_not_op2_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b0111;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b0111;
+//     })
+//   endtask
+// endclass
 
-class alu_shr1_op1_sequence extends alu_seq;
+// class alu_shr1_op1_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_shr1_op1_sequence)
+//   `uvm_object_utils(alu_shr1_op1_sequence)
 
-  function new(string name = "alu_shr1_op1_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_shr1_op1_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b1000;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b1000;
+//     })
+//   endtask
+// endclass
 
-class alu_shl1_op1_sequence extends alu_seq;
+// class alu_shl1_op1_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_shl1_op1_sequence)
+//   `uvm_object_utils(alu_shl1_op1_sequence)
 
-  function new(string name = "alu_shl1_op1_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_shl1_op1_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b1001;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b1001;
+//     })
+//   endtask
+// endclass
 
-class alu_shr1_op2_sequence extends alu_seq;
+// class alu_shr1_op2_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_shr1_op2_sequence)
+//   `uvm_object_utils(alu_shr1_op2_sequence)
 
-  function new(string name = "alu_shr1_op2_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_shr1_op2_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b1010;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b1010;
+//     })
+//   endtask
+// endclass
 
-class alu_shl1_op2_sequence extends alu_seq;
+// class alu_shl1_op2_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_shl1_op2_sequence)
+//   `uvm_object_utils(alu_shl1_op2_sequence)
 
-  function new(string name = "alu_shl1_op2_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_shl1_op2_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b1011;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b1011;
+//     })
+//   endtask
+// endclass
 
-class alu_ror_op1_op2_sequence extends alu_seq;
+// class alu_ror_op1_op2_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_ror_op1_op2_sequence)
+//   `uvm_object_utils(alu_ror_op1_op2_sequence)
 
-  function new(string name = "alu_ror_op1_op2_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_ror_op1_op2_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b1100;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b1100;
+//     })
+//   endtask
+// endclass
 
-class alu_rol_op1_op2_sequence extends alu_seq;
+// class alu_rol_op1_op2_sequence extends alu_seq;
 
-  `uvm_object_utils(alu_rol_op1_op2_sequence)
+//   `uvm_object_utils(alu_rol_op1_op2_sequence)
 
-  function new(string name = "alu_rol_op1_op2_sequence");
-    super.new(name);
-  endfunction
+//   function new(string name = "alu_rol_op1_op2_sequence");
+//     super.new(name);
+//   endfunction
 
-  virtual task body();
-    `uvm_do_with(txn, {
-      txn.mode == 1'b0;
-      txn.cmd ==4'b1101;
-    })
-  endtask
-endclass
+//   virtual task body();
+//     `uvm_do_with(txn, {
+//       txn.mode == 1'b0;
+//       txn.cmd ==4'b1101;
+//     })
+//   endtask
+// endclass
 
 //------------------------------------------------------------------------------
 // Class       : set_cout_seq

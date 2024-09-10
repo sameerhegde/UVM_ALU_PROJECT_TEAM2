c4d3b57a1792db5b7eb2d70bcae9f7ec811f6f24
AnweshRShetty <141901648+AnweshRShetty@users.noreply.github.com>
2024-09-10 09:59:47 +0530

    Update alu_driver.sv

--- alu_driver.sv
+++ alu_driver.sv
@@ -7,6 +7,8 @@
 //------------------------------------------------------------------------------
 // Copyright    : 2024(c) Manipal Center of Excellence. All rights reserved.
 //------------------------------------------------------------------------------
+
+
 class alu_drv extends uvm_driver #(alu_seq_item);
 
   `uvm_component_utils (alu_drv);
@@ -28,12 +30,13 @@ class alu_drv extends uvm_driver #(alu_seq_item);
   virtual task run_phase(uvm_phase phase);
     forever
         begin
-          `uvm_info("DRIVER","driver start",UVM_LOW)
-          repeat(2) @(posedge vif.DRV.drv_cb);
+          //repeat(2) @(posedge vif.DRV.drv_cb);
+         //begin
           seq_item_port.get_next_item (txn);
           drive();   
           seq_item_port.item_done ();
-        end
+         //end
+    end
     
   endtask: run_phase
 
@@ -42,6 +45,7 @@ class alu_drv extends uvm_driver #(alu_seq_item);
  virtual task drive();
     if (vif.DRV.drv_cb.rst)
       begin
+        repeat(1) @(posedge vif.DRV.drv_cb);
         vif.DRV.drv_cb.ce <= 'bz;
         vif.DRV.drv_cb.cin <= 'bz;
         vif.DRV.drv_cb.cmd <= 'bz;
@@ -49,9 +53,11 @@ class alu_drv extends uvm_driver #(alu_seq_item);
         vif.DRV.drv_cb.opb <= 'bz;
         vif.DRV.drv_cb.mode <= 'bz;
         vif.DRV.drv_cb.inp_valid <= 'bz;
-      end
+        //repeat(1) @(posedge vif.DRV.drv_cb);
+        end
     else
       begin
+       repeat(1) @(posedge vif.DRV.drv_cb);
        vif.DRV.drv_cb.ce <= txn.ce; 
        vif.DRV.drv_cb.cin <= txn.cin;
        vif.DRV.drv_cb.cmd <= txn.cmd;     
@@ -59,8 +65,10 @@ class alu_drv extends uvm_driver #(alu_seq_item);
        vif.DRV.drv_cb.opb <= txn.opb;
        vif.DRV.drv_cb.mode <= txn.mode;
        vif.DRV.drv_cb.inp_valid <= txn.inp_valid;
-     end
-   `uvm_info("DRIVER",$sformatf(" [%0t] mode = %d ip_valid = %d  cmd = %d  opa = %d opb = %d  ce = %d cin = %d",$time,vif.DRV.drv_cb.mode,vif.DRV.drv_cb.inp_valid,vif.DRV.drv_cb.cmd,vif.DRV.drv_cb.opa,vif.DRV.drv_cb.opb,vif.DRV.drv_cb.ce,vif.DRV.drv_cb.cin),UVM_LOW)
+        //repeat(1) @(posedge vif.DRV.drv_cb);
+   `uvm_info("DRIVER",$sformatf(" [%0t] mode = %d ip_valid = %d  cmd = %d  opa = %d opb = %d delay = %d  ce = %d cin = %d",$time,txn.mode,txn.inp_valid,txn.cmd,txn.opa,txn.opb,txn.delay,txn.ce,txn.cin),UVM_LOW)
+   repeat(1) @(posedge vif.DRV.drv_cb);
+      end
   endtask
   
 endclass: alu_drv
